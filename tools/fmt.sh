980ff2982f8451196016f7760b5c4a02fbcdd6d6
maciej <maciekursyn@gmail.com>
2026-08-18 01:28:28 +0200

    fix fmt.sh, add tmp* to .gitignore

--- tools/fmt.sh
+++ tools/fmt.sh
@@ -6,16 +6,16 @@ opt=${1-default}
 set -euo pipefail
 
 if [ "$opt" = "x" ] || [ "$opt" = "exec" ]; then
-    echo -e "writing the formatted output"$$opt"n"
+    echo -e "writing the formatted output $opt \n"
     gofmt -w .
 fi
 
 if [ "$opt" = "l" ] || [ "$opt" = "list" ]; then
-    echo -e "listing the source files that need formating"$$opt"n"
+    echo -e "listing the source files that need formating $opt \n"
     gofmt -l .
 fi
 
 if [ "$opt" = "d" ] || [ "$opt" = "diff" ]; then
-    echo -e "show the diff of needed changes and current state$$optn"
+    echo -e "show the diff of needed changes and current state $opt \n"
     gofmt -d .
 fi
