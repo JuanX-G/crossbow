a1e8623338a432fde2ac4bd9cf6fe9e1a12ecfd2
maciej <maciekursyn@gmail.com>
2026-08-17 18:31:11 +0200

    added comments to scripts

--- tools/fmt.sh
+++ tools/fmt.sh
@@ -1,21 +1,21 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 echo -e "formating code"
 
 opt=${1-default}
 set -euo pipefail
 
-if [ $opt = "x" ] || [ $opt = "exec" ]; then 
-    echo -e "writing the formatted output\n"
+if [ "$opt" = "x" ] || [ "$opt" = "exec" ]; then
+    echo -e "writing the formatted output"$$opt"n"
     gofmt -w .
 fi
 
-if [ $opt = "l" ] || [ $opt = "list" ]; then 
-    echo -e "listing the source files that need formating\n"
+if [ "$opt" = "l" ] || [ "$opt" = "list" ]; then
+    echo -e "listing the source files that need formating"$$opt"n"
     gofmt -l .
 fi
 
-if [ $opt = "d" ] || [ $opt = "diff" ]; then 
-    echo -e "show the diff of needed changes and current state\n"
+if [ "$opt" = "d" ] || [ "$opt" = "diff" ]; then
+    echo -e "show the diff of needed changes and current state$$optn"
     gofmt -d .
 fi
--- tools/testall.sh
+++ tools/testall.sh
@@ -1,19 +1,22 @@
-#!/usr/bin/env bash 
-
-echo -e "running the test suite\n"
+#!/usr/bin/env bash
 
 clean=${1-default}
 benchmark=${2-default}
 
 set -euo pipefail
 
-if [ $clean = "c" ]; then 
+if [ "$clean" = "c" ]; then # Clean test cache and run tests
+    echo -e "running the test suite\n"
     go clean -testcache
+    go test -v -race ./...
 fi
 
-go test -v -race ./...
+if [ "$clean" = "x" ]; then # just eXecute the tests
+    echo -e "running the test suite\n"
+    go test -v -race ./...
+fi
 
-if [ $benchmark = "b" ]; then
+if [ "$benchmark" = "b" ]; then  # run Benchmarks
     echo -e "running the benchmarks\n"
     go test -bench=. -benchmem
 fi
