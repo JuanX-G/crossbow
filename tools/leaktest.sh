6e24b6fd1b85b07d6d515460c51d13dbc2ff1222
maciej <maciekursyn@gmail.com>
2026-08-17 18:31:11 +0200

    added comments to scripts

--- tools/leaktest.sh
+++ tools/leaktest.sh
@@ -1,25 +1,25 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 echo -e "testing goroutine leaks\n"
 
 clean=${1-default}
 set -euo pipefail
 
-if [ $clean = "c" ]; then 
+if [ "$clean" = "c" ]; then # Clean the test cache
     go clean -testcache
 fi
 
-go test -tags=leaktest -c -o tests # Leak test is the builtag for including the detector
+go test -tags=leaktest -c -o tests # 'leaktest' is the builtag for including the detector
 
 failed=0
 fail() {
-    $failed=1
-    local test = $1
+    failed=1
+    local test=$1
     echo "[!!]: $test failed"
 }
 
 for test in $(go test -list . | grep -E "^(Test|Example)"); do ./tests -test.run "^$test\$" &>/dev/null && echo -e "$test passed." || (fail $test); done
 
-if [ $failed -eq 1 ]; then 
+if [ $failed -eq 1 ]; then
     echo "FAIL"
 fi
--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -5,9 +5,7 @@ echo -e "# running the pre commit script"
 set -euo pipefail
 
 ./tools/fmt.sh x
-
 ./tools/testall.sh c b
-
 ./tools/leaktest.sh c
 
 echo -e "\n # pre-commit script done"
