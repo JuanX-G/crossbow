a19bde6cdb918f52d8b6fc4ea8261482f2ea0e3d
maciej <maciekursyn@gmail.com>
2026-08-19 00:41:45 +0200

    Updated the registry

--- registry/registry.go
+++ registry/registry.go
@@ -3,23 +3,23 @@ package registry
 import (
 	"sync"
 
-	. "github.com/JuanX-G/crossbow"
+	"github.com/JuanX-G/crossbow"
 )
 
 // hold a сancel func and a server pointer.
-type cancelableServer[T ServerHandler[M, O], M, O any] struct {
-	server *Server[T, M, O]
+type cancelableServer[T crossbow.ServerHandler[M, O], M, O any] struct {
+	server *crossbow.Server[T, M, O]
 	cancel func()
 }
 
 // Registry holds a map of like cancelable servers keyed by the type K.
-type Registry[K comparable, T ServerHandler[M, O], M, O any] struct {
+type Registry[K comparable, T crossbow.ServerHandler[M, O], M, O any] struct {
 	mu      sync.RWMutex
 	servers map[K]*cancelableServer[T, M, O]
 }
 
 // Returns an empty registry.
-func NewRegistry[K comparable, T ServerHandler[M, O], M any, O any]() *Registry[K, T, M, O] {
+func NewRegistry[K comparable, T crossbow.ServerHandler[M, O], M any, O any]() *Registry[K, T, M, O] {
 	return &Registry[K, T, M, O]{
 		servers: make(map[K]*cancelableServer[T, M, O]),
 	}
@@ -28,7 +28,7 @@ func NewRegistry[K comparable, T ServerHandler[M, O], M any, O any]() *Registry[
 // Register a server in a registry. Cancel should be a function
 // that cancels the srv argument. Returns an error if the key
 // already exists or if cancel is nil.
-func (s *Registry[K, T, M, O]) RegisterServer(cancel func(), k K, srv *Server[T, M, O]) error {
+func (s *Registry[K, T, M, O]) RegisterServer(cancel func(), k K, srv *crossbow.Server[T, M, O]) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if _, ok := s.servers[k]; ok {
@@ -58,9 +58,12 @@ func (s *Registry[K, T, M, O]) Remove(k K) bool {
 
 // Looks up the server under the key k. If the key
 // does not exist, returned is nil and false.
-func (s *Registry[K, T, M, O]) Resolve(k K) (*Server[T, M, O], bool) {
-	s.mu.Lock()
+func (s *Registry[K, T, M, O]) Resolve(k K) (*crossbow.Server[T, M, O], bool) {
+	s.mu.RLock()
 	managedSrv, ok := s.servers[k]
-	s.mu.Unlock()
-	return managedSrv.server, ok // .server will be nil if the lookup failes and ok == false.
+	s.mu.RUnlock()
+	if !ok {
+		return nil, false
+	}
+	return managedSrv.server, ok
 }
